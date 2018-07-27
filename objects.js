e295a6c57e83d18cc80d6bd87ec4342314637a1e
Daniel Schoch <d.schoch90@gmail.com>
2018-07-27 23:43:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,14 @@
-var playlist = {artistName: songTitle};
+var playlist = {
+  'Foo Fighters': 'Everlong',
+  Prince: 'Purple Rain'
+}
 
-function updatePlaylist (playlist, artistName, songTitle) {
-  return playlist.artistName = 'songTitle';
+function updatePlaylist(playlist, artist, song) {
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist]
+
+  return playlist
 }
